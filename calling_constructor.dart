77e0811f479a1f3b513132371f9074b0f888b7a8
Avijit Mandal <46240269+avijit1999@users.noreply.github.com>
2020-09-15 18:20:08 +0530

    Add files via upload

---
+++ calling_constructor.dart
@@ -0,0 +1,23 @@
+import 'construct.dart';
+
+class ConstList {
+  String name;
+  int rollNo;
+
+  ListName(int tileNo) {
+    return student[tileNo].name;
+  }
+
+  listRollNo(int tileNo) {
+    return student[tileNo].rollNo;
+  }
+
+  List student = [
+    Constractor("Avijit", 111),
+    Constractor("Avijit2", 222),
+    Constractor("Avijit3", 333),
+    Constractor("Avijit4", 444),
+    Constractor("Avijit5", 555),
+    Constractor("Avijit6", 666),
+  ];
+}
